14efa613074b9dfb16fcbde134ecc2220ca998d4
mruddhabcharandas <mr.uddhabcharandas@gmail.com>
2026-06-11 07:41:43 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -54,7 +54,7 @@ app.use(cors({
     "https://www.smartodisha.in"
   ],
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+  methods: ["GET", "POST", "PUT","PATCH","DELETE", "OPTIONS"]
 }));
 app.use(express.json());
 app.use(morgan("dev"));
